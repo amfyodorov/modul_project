b8447973ba541f58fe146f5a8d821189f02ebe45
amfyodorov <amfyodorov@edu.hse.ru>
2023-06-20 21:41:05 +0300

    first commit

---
+++ Client2/Client2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h> // Для функции InetPton
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_BUFFER_SIZE 1024
+
+#include <sstream>
+
+template <typename T>
+std::string NumberToString(T Number)
+{
+    std::ostringstream ss;
+    ss << Number;
+    return ss.str();
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        std::cerr << "Ошибка при инициализации Winsock!" << std::endl;
+        return -1;
+    }
+
+    // Создание сокета
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Ошибка при создании сокета!" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    // Установка параметров адреса сервера
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8888);
+
+    // Преобразование IP-адреса с помощью InetPton
+    if (InetPtonA(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) <= 0)
+    {
+        std::cerr << "Некорректный IP-адрес!" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // Подключение к серверу
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        std::cerr << "Ошибка при подключении к серверу!" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Подключение к серверу установлено." << std::endl;
+
+    // Буфер для приема данных от сервера
+    char buffer[MAX_BUFFER_SIZE] = { 0 };
+
+    // Получение сообщения от сервера
+    recv(clientSocket, buffer, MAX_BUFFER_SIZE, 0);
+    std::cout << "Сообщение от сервера: " << buffer << std::endl;
+
+    // Ввод числа
+    std::cout << "Введите число от 0 до 100: ";
+    int guess;
+    std::cin >> guess;
+
+    // Отправка числа на сервер
+    std::string message = NumberToString<int>(guess);
+    if (send(clientSocket, message.c_str(), message.length(), 0) == SOCKET_ERROR)
+
+        // Получение результата от сервера
+        memset(buffer, 0, sizeof(buffer));
+   int a = recv(clientSocket, buffer, MAX_BUFFER_SIZE, 0);
+   buffer[a] = '\0';
+
+    // Вывод результата
+    std::cout << "Результат: " << buffer << std::endl;
+
+    // Закрытие сокета и завершение программы
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <cstring>
+#include <winsock2.h>
+#include <cstdlib>
+#include <ctime>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_BUFFER_SIZE 1024
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        std::cerr << "Ошибка при инициализации Winsock!" << std::endl;
+        return -1;
+    }
+
+    // Создание сокета
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Ошибка при создании сокета!" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    // Установка параметров адреса сервера
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8888);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Привязка сокета к адресу сервера
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        std::cerr << "Ошибка при привязке сокета!" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // Прослушивание подключений
+    if (listen(serverSocket, 2) == SOCKET_ERROR)
+    {
+        std::cerr << "Ошибка при прослушивании сокета!" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Сервер запущен и ожидает подключений." << std::endl;
+
+    // Принятие подключений от клиентов
+    SOCKET clientSocket1 = accept(serverSocket, NULL, NULL);
+    SOCKET clientSocket2 = accept(serverSocket, NULL, NULL);
+
+    std::cout << "Подключение к двум клиентам установлено." << std::endl;
+
+    // Генерация случайного числа
+    int secretNumber = rand() % 101;
+    for (int i = 0; i < rand() % 8; i++)
+        secretNumber = (secretNumber * rand()) % 101;
+    std::cout << "Сгенерированное число: " << secretNumber << std::endl;
+
+    // Отправка сгенерированного числа обоим клиентам
+    std::string message = "Угадайте число от 0 до 100:";
+    send(clientSocket1, message.c_str(), message.length(), 0);
+    send(clientSocket2, message.c_str(), message.length(), 0);
+
+    // Буферы для приема данных от клиентов
+    char buffer1[MAX_BUFFER_SIZE];
+    char buffer2[MAX_BUFFER_SIZE];
+
+    // Получение и сравнение чисел от клиентов
+    recv(clientSocket1, buffer1, MAX_BUFFER_SIZE, 0);
+    recv(clientSocket2, buffer2, MAX_BUFFER_SIZE, 0);
+
+    int guess1 = atoi(buffer1);
+    int guess2 = atoi(buffer2);
+
+    int diff1 = abs(guess1 - secretNumber);
+    int diff2 = abs(guess2 - secretNumber);
+
+    std::string result;
+
+    if (diff1 < diff2)
+    {
+        result = "Игрок 1 победил!";
+        send(clientSocket1, "Вы выйграли!", 13, 0);
+        send(clientSocket2, "Вы проиграли.", 14, 0);
+    }
+    else if (diff2 < diff1)
+    {
+        result = "Игрок 2 победил!";
+        send(clientSocket1, "Вы проиграли.", 14, 0);
+        send(clientSocket2,"Вы победили!", 13, 0);
+    }
+    else
+    {
+        result = "Ничья!";
+        send(clientSocket1, result.c_str(), result.length(), 0);
+        send(clientSocket2, result.c_str(), result.length(), 0);
+    }
+
+    std::cout << "Результат: " << result << std::endl;
+
+    // Закрытие сокетов и завершение программы
+    closesocket(clientSocket1);
+    closesocket(clientSocket2);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
